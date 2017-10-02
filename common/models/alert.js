aca1914b0ba090813d81210df43355eafda6942c
Harshit Anand <harshitanand94@gmail.com>
2017-10-03 04:35:49 +0530

    remote method for alert model added & linting changes made to codebase

--- common/models/alert.js
+++ common/models/alert.js
@@ -1,3 +1,13 @@
-'use strict';
+"use strict";
 
-module.exports = Alert => {};
+module.exports = Alert => {
+  Alert.getAllAlerts = async () => {
+    const alertFilter = {
+      where: {
+        active: false,
+      },
+    };
+    const alerts = await Alert.find(alertFilter);
+    return alerts;
+  };
+};
